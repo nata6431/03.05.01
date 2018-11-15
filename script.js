a6091534bc80906552e974c35881958b8510cee3
Caroline Bredal Malmgren <carolinebredalmalmgren@Carolines-MacBook-Pro.local>
2018-11-15 12:41:00 +0100

    starttissemand

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 //Caroline start//
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+    showStart();
+}
+
+function showStart() {
+    console.log("show start");
+    startGame();
+
+}
 
 function startGame() {
     console.log("start game");
-    document.querySelector("#fig1").classList.remove(hide);
+    document.querySelector("#fig1").classList.remove("hide");
     document.querySelector("#fig1").addEventListener("click", showDick);
 
 }
 
 function showDick() {
-    document.querySelector("#fig1").classList.add(hide);
-    document.querySelector("#fig2").classList.remove(hide);
+    document.querySelector("#fig1").classList.add("hide");
+    document.querySelector("#fig2").classList.remove("hide");
 }
 
 //Caroline slut//
